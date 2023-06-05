ad258c378093392da0a9fc5526b8630a0e531905
Khoavuanh <khoa.vuanh@hotmail.com>
2023-06-04 20:03:04 -0700

    please review

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 class Casino {
     // Write code here
+    constructor(name, fakeCoin){
+        this.name=name;
+        this.fakeCoin=false;
+        this.timesPlayed = 0;
+        this.winning = 0;
+    }
+    playGame(betAmount){
+        this.timesPlayed += 1 ;
+        if (Math.random() > 0.5 && this.fakeCoin == false) {
+            console.log("You won" + betAmount * (this.timesPlayed) + "dollar");
+            this.winning += this.timesPlayed * betAmount;
+        }
+        else {
+            console.log (this.name + "wins!");
+            this.timesPlayed= 0;
+            this.winning =0;
+        }
+    }
+      rollDice(sides) {
+    console.log(Math.floor(Math.random() * sides) + 1);
+  }
+    
+    
 };
 
 // TESTS
@@ -8,7 +31,8 @@ console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
-myCasino.playGame(35);
+myCasino.playGame(50);
+
 
 // BONUS TESTS
 /*
